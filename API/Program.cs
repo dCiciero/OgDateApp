a7daeb509337121df9994920d1d27e5a73959731
Ogaga Ivhurie <ivhurie01@yahoo.co.uk>
2024-10-29 16:56:21 +0000

    added auth

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,12 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +14,10 @@ app.UseCors(opt => {
     opt.AllowAnyHeader().AllowAnyMethod()
         .WithOrigins("http://localhost:4200","https://localhost:4200");
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
