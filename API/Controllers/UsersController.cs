2fd094bc5ae1d96a7fadc5ebe32c47203f21b277
Ogaga Ivhurie <ivhurie01@yahoo.co.uk>
2025-02-18 01:37:06 +0000

    resources

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -29,6 +30,23 @@ namespace API.Controllers
                 return BadRequest("User not found");
             }
             return user;
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await userRepository.GetUserByUsernameAsync(username);
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+            mapper.Map(memberUpdateDto, user);
+            // userRepository.Update(user);
+            if (await userRepository.SaveAllAsync())
+            {
+                return NoContent();
+            }
+            return BadRequest("Failed to update user");
         }
     }
 }
